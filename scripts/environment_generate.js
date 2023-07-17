151b863acccaa1fd2f98012cd35a2e05ae5b243f
thiagotar <thiago-tar@hotmail.com>
2023-07-17 21:09:01 +0930

    folder environment has been created

--- scripts/environment_generate.js
+++ scripts/environment_generate.js
@@ -9,7 +9,11 @@ function generateEnvironmentContent() {
   const fs = require('fs');
   const fileName = 'environment.ts'; // you can this as hard coded name, or you can use your own unique name
   const content = generateEnvironmentContent();
+  const folderName = `src/environments`;
+  if (!fs.existsSync(folderName)) {
+    fs.mkdirSync(folderName);
+  }
+  process.chdir(folderName); // This is the directory where you created the environment file. you can use your own path, but for this I used the Angular default environment directory
   console.log(content)
-  process.chdir(`src/environments`); // This is the directory where you created the environment file. you can use your own path, but for this I used the Angular default environment directory
   fs.writeFile(fileName, content, (err) => { (err) ? console.log(err) : console.log('env is generated!') });
   })();
